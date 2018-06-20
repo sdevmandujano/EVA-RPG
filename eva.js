83e7e98157b5c4b3c6f81e81b27c2d7bf4029d1b
sdevmandujano <zmandujano01@gmail.com>
2018-06-19 21:23:08 -0500

    update

--- eva.js
+++ eva.js
@@ -7,10 +7,11 @@
 
 //Defining the Angels(enemies)
 
-function Angel(name,lifepoints,normalattack,criticalattack)
+function Angel(name,hitpoints,normalattack,criticalattack)
    this.name = name;
-   this.lifepoints;
-   this.attackpoints;
+   this.hitpoints = hitpoints;
+   this.attackpoints = attackpoints;
+   this.defensepoints = defensepoints;
    this.attack = function () {
 //work in the attack function
    }
@@ -25,21 +26,22 @@ this.status = function () {
         console.log("Enemy Defeated");
     } 
 }
+
+//Object Ramiel
 var ramiel = new Angel("Ramiel", 30, 8, Math.floor((Math.random() * 10) + 1) )
 
 //Declaring the Evangelion Units(character's to select) 
 
 function EVA(name,lifepoints,normalattack,criticalattack)
   this.name = name;
-  this.lifepoints = lifepoints;
-  this.normalattack = normalttack;
+  this.hitpoints = hitpoints;
+  this.normalattack = normalattack;
   this.criticalattack = criticalattack;
   this.attack = function () {
-  //work in the attack function    
+  ramiel.hitpoints - EVA.normalattack;  
   }
 
   this.defend = function () {
-//work in the defend function 
   }
   this.critical = function () {
 //work in the critical function
@@ -50,6 +52,26 @@ function EVA(name,lifepoints,normalattack,criticalattack)
       }
   }
 
+//Object Unit01
 var unit01 = new EVA("Unit01", 40, 5, Math.floor((Math.random() * 10) + 1))
+
+
+//Animating Angel 
+
+//Appears, its Alive
+$(document).ready(function(){
+    $(".btn1").click(function(){
+        $("p").fadeIn()
+    });
+
+//Defeated
+$(".btn2").click(function(){
+    $("p").fadeOut(); //check this out https://api.jquery.com/fadeOut/
+});
+});
+
+//Attacks
+
+
 
 
